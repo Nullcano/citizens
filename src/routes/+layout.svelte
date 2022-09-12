283563ec55f1ad0619d9de6a3cb905c1c555901f
Nullcano <mo@shi.moe>
2022-09-13 00:28:52 +0200

    logged in feed concept

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../styles/sweet.css';
+	import '../styles/fonts.css';
+	import '../styles/global.css';
+  import AppHeader from '../lib/AppHeader.svelte';
+</script>
+
+<main>
+  <AppHeader />
+  <slot />
+</main>
+
+<style>
+  main {
+    margin: 0 auto;
+    max-width: 60rem;
+    width: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let names = ['Aiden','Arion','Edard','Patrock','Harryx','Davon','Waelon','Rohn','Brayln','Maddax']
+  const randomItem = (arr) => arr[(Math.random() * arr.length) | 0];
+</script>
+
+<div class="greeting-container">
+  <div class="greeting">Good morning,</div>
+  <div class="name">{randomItem(names)}</div>
+</div>
+
+<div class="search-container">
+  <input type="text" placeholder="Search #tags @locations and /names ..." />
+</div>
+
+<div class="feed">
+  <div class="controls">
+    <div class="label">Mode</div>
+    <div class="buttons">
+      <div class="button">National</div>
+      <div class="button selected">International</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .greeting-container {
+    position: relative;
+    margin: 0 auto;
+    max-width: 60rem;
+    width: 100%;
+    font-size: 1.25rem;
+  }
+  .greeting, .name {
+    margin: 0 2rem;
+  }
+  .name {
+    color: var(--lightest);
+    font-weight: 500;
+  }
+  .search-container {
+    position: relative;
+    display: grid;
+    z-index: 20;
+  }
+  input {
+    margin: 2rem;
+    padding: 1rem;
+    border-radius: 1rem;
+    font-size: 1rem;
+    background-color: var(--dark);
+    color: var(--lightest);
+    border: 0;
+    outline: .5rem solid var(--darkest);
+  }
+  input:focus {
+    outline: .5rem solid var(--darker);
+  }
+  .controls {
+    margin: 0 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: .5rem;
+  }
+  .label {
+    font-size: 1.25rem;
+    color: var(--lightest);
+  }
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    gap: .5rem;
+  }
+  .button {
+    display: inline-block;
+    padding: .2rem .6em;
+    border-radius: 999px;
+    border: 2px solid var(--darker);
+    cursor: pointer;
+  }
+  .button.selected {
+    background: var(--darker);
+  }
+</style>
